bf3632025ef785b3779711f372f220c26c1858ce
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-02-19 19:14:53 -0800

    API routes for creating workout and getting workouts in range created.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,10 +10,19 @@ router.get("/api/workouts", (req, res) => {
   });
 });
 
-router.post("/api/workouts",({body}, res) => {
+router.put("/api/workouts",({body}, res) => {
   Workout.create(body)
   .then(({_id}) => Workout.findOneAndUpdate({}, { $push: { exercise: _id}}, { new: true}))
   .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.post("/api/workouts/range",(req, res) => {
+  Workout.find({}).then(dbWorkout => {
     res.json(dbWorkout);
   })
   .catch(err => {
